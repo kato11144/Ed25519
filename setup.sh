3b50448a9b774bab7c07dbcd3c10ba34879db0da
kato11144 <ts4.29r@gmail.com>
2024-12-03 16:33:29 +0900

    remove setup.sh

--- setup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-export C_INCLUDE_PATH=/opt/homebrew/Cellar/libsodium/1.0.20/include:$C_INCLUDE_PATH
-export LIBRARY_PATH=/opt/homebrew/Cellar/libsodium/1.0.20/lib:$LIBRARY_PATH
-
-echo "Environment variables set for the project."
